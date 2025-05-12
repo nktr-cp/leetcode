2cc244e74679be8694c881cde115b5e49afdba01
nktr-cp <nktrcp@gmail.com>
2025-05-12 23:31:12 +0900

    step3

---
+++ step3.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> number_to_index;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (number_to_index.contains(target - nums[i])) {
+                return vector<int>({number_to_index[target - nums[i]], i});
+            }
+            number_to_index[nums[i]] = i;
+        }
+        assert(false);
+    }
+};
