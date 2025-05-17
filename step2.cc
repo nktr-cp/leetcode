ecb0d87605388b05c9c843f9786bd21fda1f2ca3
nktr-cp <nktrcp@gmail.com>
2025-05-17 17:14:41 +0900

    step2.cc

---
+++ step2.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> sum_count;
+        
+        int current_sum = 0;
+        ++sum_count[current_sum];
+        
+        int valid_subarray_count = 0;
+        for (int num : nums) {
+            current_sum += num;
+            
+            if (sum_count.contains(current_sum - k)) {
+                valid_subarray_count += sum_count[current_sum - k];
+            }
+            ++sum_count[current_sum];
+        }
+        
+        return valid_subarray_count;
+    }
+};
